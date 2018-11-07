1b7d2bee6250d2a2ff6afa1ad3e650d105ef9965
iskou9821 <kou.ishikawa@gmail.com>
2018-11-07 14:08:25 +0900

    トークン無効化のAPIでセッション情報を利用するように修正

--- lib/chikyu/sdk/resource/security_token.rb
+++ lib/chikyu/sdk/resource/security_token.rb
@@ -18,11 +18,12 @@ module Chikyu::Sdk
       item
     end
 
-    def self.revoke(token_name, login_token, login_secret_token)
-      OpenResource.invoke(path: '/session/token/revoke',
-                          data: {token_name: token_name,
-                                 login_token: login_token,
-                                 login_secret_token: login_secret_token})
+    def self.revoke(token_name, login_token, login_secret_token, session)
+      s = SecureResource.new session
+      s.invoke(path: '/session/token/revoke',
+               data: {token_name: token_name,
+                      login_token: login_token,
+                      login_secret_token: login_secret_token})
       item
     end
   end
